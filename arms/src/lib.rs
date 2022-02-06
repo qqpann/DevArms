01edfadb027a880c96938a8c219d0730ac5bff81
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-02-07 04:34:29 +0900

    feat: add sha256 hash in arms lib

---
+++ arms/src/lib.rs
@@ -0,0 +1,22 @@
+use sha2::{Digest, Sha256};
+// use base64ct::{Base64, Encoding};
+
+// Thanks: https://github.com/RustCrypto/hashes
+
+pub fn sha256(txt: &str) -> String {
+    let hash = Sha256::new().chain_update(txt).finalize();
+    // Base64::encode_string(&hash)
+    base16ct::lower::encode_string(&hash)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn hash_works() {
+        let txt = "Hello World";
+        assert_eq!(
+            crate::sha256(txt),
+            "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e".to_string()
+        );
+    }
+}
