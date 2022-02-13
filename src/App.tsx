17c53d1c2e23159e68a04b468e7ea6c21d1f738b
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-02-13 21:57:01 +0900

    add: color switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { toolPages } from './Pages'
 
 function App() {
   return (
     <div className="App">
       <nav>
+        <ColorModeSwitcher />
         {toolPages.map((page) => (
           <Link to={page.path} key={page.path}>
             {page.name}
