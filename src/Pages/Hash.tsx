65ddb1a8d412230483b383c425ba896c895c664b
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-02-14 01:27:57 +0900

    add: clear source button

--- src/Pages/Hash.tsx
+++ src/Pages/Hash.tsx
@@ -1,15 +1,15 @@
-import React, { SyntheticEvent, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import init, { sha256 } from 'arms'
 import { ArmsInput } from '../components/Input/Input'
 import { ArmsOutput } from '../components/Output/Output'
 import { Heading, VStack } from '@chakra-ui/react'
 
 export const HashGenerator = () => {
-  const { source, onChangeSource, sha256str } = useHash()
+  const { source, update, sha256str } = useHash()
   return (
     <VStack alignItems="start" w="full">
       <Heading size="md">Input</Heading>
-      <ArmsInput value={source} onChange={onChangeSource} />
+      <ArmsInput value={source} set={update} />
       <Heading size="md">SHA256</Heading>
       <ArmsOutput value={sha256str} />
     </VStack>
@@ -31,10 +31,5 @@ const useHash = () => {
     setSha256(sha256(txt))
   }
 
-  const onChangeSource = (e: SyntheticEvent) => {
-    const target = e.target as HTMLInputElement
-    update(target.value)
-  }
-
-  return { source, onChangeSource, sha256str }
+  return { source, update, sha256str }
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import {
   Input,
   InputProps,
@@ -7,17 +7,27 @@ import {
   Button,
   Text,
 } from '@chakra-ui/react'
-import { FaPaste } from 'react-icons/fa'
+import { IoCloseCircle } from 'react-icons/io5'
 
-type Props = InputProps
-export const ArmsInput = (props: Props) => {
+type Props = InputProps & {
+  value: string
+  set: (txt: string) => void
+}
+export const ArmsInput = ({ value, set, ...rest }: Props) => {
+  const onChange = (e: SyntheticEvent) => {
+    const target = e.target as HTMLInputElement
+    set(target.value)
+  }
+  const onClear = () => {
+    set('')
+  }
   return (
     <InputGroup>
-      <Input {...props} size="lg" pr="4rem" />
+      <Input {...rest} value={value} onChange={onChange} size="lg" pr="4rem" />
       <InputRightElement h="100%" alignItems="center" ml="1" mr="2">
-        <Button>
+        <Button onClick={onClear} bg="inherit">
           <Text>
-            <FaPaste />
+            <IoCloseCircle />
           </Text>
         </Button>
       </InputRightElement>
