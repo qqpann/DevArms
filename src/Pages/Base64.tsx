1833a89d73578693811710c041379a1a9d34658b
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-02-14 04:35:54 +0900

    add: Base64 encoder decoder page

--- src/Pages/Base64.tsx
+++ src/Pages/Base64.tsx
@@ -1,5 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import init, { base64enc, base64dec } from 'arms'
+import { ArmsInput } from '../components/Input/Input'
+import { ArmsOutput } from '../components/Output/Output'
+import { Heading, VStack } from '@chakra-ui/react'
 
 export const Base64Converter = () => {
-  return <>Base64 converter</>
+  const { source, base64, updateSource, updateBase64 } = useBase64()
+  return (
+    <VStack alignItems="start" w="full">
+      <Heading size="md">Plain text</Heading>
+      <ArmsInput value={source} set={updateSource} />
+      <Heading size="md">Base64</Heading>
+      <ArmsInput value={base64} set={updateBase64} />
+    </VStack>
+  )
+}
+
+const useBase64 = () => {
+  const [source, setSource] = useState<string>('Hello World')
+  const [base64, setBase64] = useState<string>('SGVsbG8gV29ybGQ=')
+
+  useEffect(() => {
+    init()
+  }, [])
+
+  const updateSource = (txt: string) => {
+    setSource(txt)
+    setBase64(base64enc(txt))
+  }
+  const updateBase64 = (txt: string) => {
+    setBase64(txt)
+    setSource(base64dec(txt))
+  }
+
+  return { source, base64, updateSource, updateBase64 }
 }
