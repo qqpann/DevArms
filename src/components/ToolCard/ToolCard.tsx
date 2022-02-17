513c8e76eda47dca25a21ed6e3dcb8319938cc6d
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-02-17 09:11:06 +0900

    update: able to navigate page with tool card

--- src/components/ToolCard/ToolCard.tsx
+++ src/components/ToolCard/ToolCard.tsx
@@ -1,27 +1,26 @@
-import { Box, BoxProps, Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { ToolPage } from 'src/Pages'
 
-type Props = BoxProps & {
-  page: ToolPage
-}
-export const ToolCard = ({ page, ...rest }: Props) => {
+export const ToolCard = ({ page }: { page: ToolPage }) => {
   return (
-    <Box
-      as="button"
-      {...rest}
-      borderWidth={1}
-      borderColor={'gray'}
-      borderRadius={16}
-      w={140}
-      h={200}
-    >
-      <Flex direction="column" alignItems="center" h="full" p={8}>
-        <Box p={8}>
-          <page.Icon size={32} />
-        </Box>
-        <Heading size="xs">{page.name}</Heading>
-      </Flex>
-    </Box>
+    <Link to={page.path}>
+      <Box
+        as="button"
+        borderWidth={1}
+        borderColor={'gray'}
+        borderRadius={16}
+        w={140}
+        h={200}
+      >
+        <Flex direction="column" alignItems="center" h="full" p={8}>
+          <Box p={8}>
+            <page.Icon size={32} />
+          </Box>
+          <Heading size="xs">{page.name}</Heading>
+        </Flex>
+      </Box>
+    </Link>
   )
 }
