1d3049ffb93fc9b1d6c2c6969533207d131cef15
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-02-17 09:46:49 +0900

    fix: view chakra-ui correctly on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/preset-create-react-app',
+    '@chakra-ui/storybook-addon',
   ],
   framework: '@storybook/react',
   core: {
