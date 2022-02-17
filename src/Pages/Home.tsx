d0738e9ebcac64b5aee3de33d0b758b7b94bad24
Qiushi Pan <17402261+qqhann@users.noreply.github.com>
2022-02-17 10:58:18 +0900

    add: grid to layout space between

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,3 +1,4 @@
+import { SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
 import { PageLayout } from 'src/Layouts/PageLayout'
 import { ToolCard } from 'src/components/ToolCard/ToolCard'
@@ -7,9 +8,11 @@ import { ToolPage } from '.'
 export const Home = ({ toolPages }: { toolPages: ToolPage[] }) => {
   return (
     <PageLayout title={'Home'}>
-      {toolPages.map((page) => (
-        <ToolCard page={page} key={page.path} />
-      ))}
+      <SimpleGrid minChildWidth={140} spacing={2}>
+        {toolPages.map((page) => (
+          <ToolCard page={page} key={page.path} />
+        ))}
+      </SimpleGrid>
     </PageLayout>
   )
 }
